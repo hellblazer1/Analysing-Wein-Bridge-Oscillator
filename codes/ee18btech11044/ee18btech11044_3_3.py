313ce0222e763c20127817a8aaf5e587563d22fc
saiashok <ee18btech11044@iith.ac.in>
2020-05-26 01:28:21 +0530

    message

--- codes/ee18btech11044/ee18btech11044_3_3.py
+++ codes/ee18btech11044/ee18btech11044_3_3.py
@@ -1,25 +1,35 @@
 import numpy as np
-from scipy import signal
 import matplotlib.pyplot as plt
-
-RC = 160 *(10**-6)
+from sympy.abc import s,t
+from sympy.integrals import inverse_laplace_transform
+import sympy as sy
+import math
+import cmath
 
 #if using termux
 import subprocess
 import shlex
-#end
+#end if
 
-num = [3.03*(RC**2),9.09*RC,3.03]	#describing transfer function
-den = [(RC**2),(-0.03)*RC,1]
-system = signal.lti(num,den)
+G, k = sy.symbols('G ,k', real=True)
 
+Ts = (G*((k)**2)*(s**2) + G*(3*k)*s + G)/(((k**2)*(s**2))+ ((3-G)*k*s) + 1)	#transfer function
+Us = 1/s
+Ys = Ts*(Us)
+y = inverse_laplace_transform(Ys,s,t) 	#inverse laplace transform
+R = 10000
+C = (16)*(10**(-9))
+y_t = y.subs({G: 3.03, k: R*C})
+#print("Step Response is :",y_t)
 
-T, yout = signal.step(system)	#step response
+x = np.linspace(0,0.2,10000)
 
-plt.plot(T,yout)
-plt.grid()
-plt.xlabel("time (t)")
-plt.title("Step system response ")
+y = -0.37879*1j*(0.0298 + 1.99974*1j)*(12.11863*np.sin(6249.2968*x) + 0.18179*1j*np.sin(6249.2968*x) + 3.9991*np.exp(-93.74994*x) + 0.059993*1j*np.exp(-93.7494*x))*np.exp(93.7494*x)*np.heaviside(x,0.5)
+
+#print(y)
+plt.xlim(0.180,0.2)
+plt.title('Step response -- Magnified plot -- f = 953.28Hz')
+plt.plot(x,y.real)
 
 #if using termux
 plt.savefig('./figs/ee18btech11044/ee18btech11044_3_3.pdf')
--- codes/ee18btech11044/ee18btech11044_3_5.py
+++ codes/ee18btech11044/ee18btech11044_3_5.py
@@ -1,24 +1,39 @@
 import numpy as np
-from scipy import signal
 import matplotlib.pyplot as plt
+from sympy.abc import s,t
+from sympy.integrals import inverse_laplace_transform
+import sympy as sy
+import math
+
 #if using termux
 import subprocess
 import shlex
-#end
+#end if
 
-RC = 160 *(10**-6)
+G, k = sy.symbols('G ,k', real=True)
 
+Ts = (G*((k)**2)*(s**2) + G*(3*k)*s + G)/(((k**2)*(s**2))+ ((3-G)*k*s) + 1)	#transfer function
+Ds = 1
+Ys = Ts*(Ds)
+y = inverse_laplace_transform(Ys,s,t) 	#inverse laplace transform
+R = 10000
+C = (16)*(10**(-9))
+y_t = y.subs({G: 3, k: R*C})
+#print("step response is :",y_t)
 
-num = [3*(RC**2),9*RC,3]	#describing transfer function
-den = [(RC**2),0,1]
-system = signal.lti(num,den)
+i = np.linspace(0,0.2,10000)
+y =[]
+for j in i:
+    y.append(3*(12*np.sin(6250.0*j) + 4)*(np.heaviside(j,0.5)/4))
 
-T, yout = signal.impulse(system)	#Impluse response
+#print(y)
+plt.xlim(0,0.02)
 
-plt.plot(T,yout)
-plt.grid()
-plt.xlabel("time (t)")
-plt.title("Impulse system response -- R2/R1 = 2")
+plt.plot(i,y)
+plt.plot(0.0133468,11.9143,'o')
+plt.plot(0.0123387,11.9143,'o')
+
+plt.title("Impulse system response -- R2/R1 = 2 -- f=991.965Hz ")
 #if using termux
 plt.savefig('./figs/ee18btech11044/ee18btech11044_3_6.pdf')
 plt.savefig('./figs/ee18btech11044/ee18btech11044_3_6.eps')
